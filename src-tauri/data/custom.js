7c61d1b47afd67b301b1918a01129d5232f51c17
grumphus <58567718+grumphus@users.noreply.github.com>
2025-07-01 02:39:47 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,5 +1,4 @@
-console.log('custom.js ----')
-
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
+console.log(
+    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
